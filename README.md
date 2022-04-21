1690eb596fe5c97a6d2e65ff674372047b911e7e
Ivan Derlich <a@ivanderlich.com>
2022-04-21 01:44:58 -0300

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn-enough-rails
 [The Michael's Hart's tutorial. 6th Edition](https://www.learnenough.com/ruby-on-rails-6th-edition-tutorial)
+
+## [Live Version](https://boiling-island-08224.herokuapp.com/)
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_04_12_231357) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
